59ba6688cb216e387d1054c77248b9e10a18e3a2
Gregor Albert <gregoralbert@gmail.com>
2017-10-29 08:12:44 +0100

    GA-4 homescreen image and responsive articles

--- webapp/src/app/app.router.ts
+++ webapp/src/app/app.router.ts
@@ -1,14 +1,15 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
-import { ServicesComponent } from './services/services.component';
+import { HomeComponent } from './home/home.component';
+import {ArticleComponent} from './article/article.component';
+import {ContactComponent} from './contact/contact.component';
 
 export const router: Routes = [
   { path: '', redirectTo: 'about', pathMatch: 'full' },
-  { path: 'about', component: AboutComponent },
-  { path: 'services', component: ServicesComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'article', component: ArticleComponent },
+  { path: 'contact', component: ContactComponent }
 ];
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(router);
